665bc1901fc04d124a723d4cfe11d5f6165cbe52
whatphilipcodes <gerdes.philip@gmail.com>
2025-01-17 11:17:26 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import p5 from "p5";
+import "./style.css";
+
+new p5((p: p5) => {
+  p.preload = () => {};
+
+  p.setup = () => {
+    p.createCanvas(500, 500);
+    p.fill(0);
+    p.stroke(255);
+    p.strokeWeight(2);
+  };
+
+  p.draw = () => {
+    p.background(0);
+    p.square(p.width * 0.5 - 50, p.height * 0.5 - 50, 100);
+  };
+});
